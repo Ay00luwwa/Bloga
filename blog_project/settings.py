d27b9168a01fb677edab00065c01337a0ec5eb44
Ay00luwwa <ijidakinroayooluwa@gmail.com>
2023-10-27 03:11:05 -0700

    media

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -145,8 +145,9 @@ STATICFILES_DIRS = [
 ]
 
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
 
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
